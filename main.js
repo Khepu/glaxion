215136d68fbf07973911ce3c8284af32948f0ad0
Giorgos Makris <makris.giwrgos1234@gmail.com>
2019-03-19 10:48:17 +0200

    Planet generation, no intersections

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 const HEIGHT = window.innerHeight;
 const WIDTH = window.innerWidth;
+const NumberOfPlanets = 30;
 
+let planets = [];
+//let color = [0xff, 0x55, 0x00]; WIP
+
+const identity = x => x;
 
 function setup() {
-    createCanvas(HEIGHT, WIDTH);
-    createPlanets(30)
-        .map(p => p()(-1))
-        .forEach(p => ellipse(p.x, p.y, radius(p), radius(p)));
+    createCanvas(WIDTH, HEIGHT);
+    tmp = initPlanets();
+
+    while(intersectingPlanets(tmp) > NumberOfPlanets){
+        tmp = initPlanets();
+    }
+
+    planets = tmp;
+    console.log(intersectingPlanets(planets));
 }
+
 
 function draw() {
-
+    clear();
+    updatePower(planets);
+    drawPlanets(planets);
 }
 
-const planet = (x, y, rate) => (power=rate*10) => owner => {
+const initPlanets = () =>
+      createPlanets(NumberOfPlanets)
+      .map(p => p(randRate())(-1));
+
+const drawPlanets = planets =>
+      planets.forEach(p => ellipse(p.x, p.y, radius(p), radius(p)));
+
+const updatePower = planets =>
+      planets.forEach(p => p.power = (p.power +  p.rate) % 120000 + 10);
+
+const planet = (x, y, rate) => (power=rate*1) => owner => {
     return {x: x,         //coordinates
             y: y,
             rate: rate,   //value increase rate
@@ -23,7 +46,7 @@ const planet = (x, y, rate) => (power=rate*10) => owner => {
 };
 
 const randomWithPad = (max, pad) =>
-      Math.trunc(Math.random() * (max-(2*pad)) + pad);
+      Math.trunc(Math.random() * (max - (2 * pad)) + pad);
 
 const randX = () =>
       randomWithPad(WIDTH, 20);
@@ -32,20 +55,30 @@ const randY = () =>
       randomWithPad(HEIGHT, 20);
 
 const randRate = () =>
-      Math.trunc(Math.random() * 20 + 5);
+      Math.trunc(Math.random() * 100 + 5);
 
 const startingPower = rate =>
       rate * 10;
 
 const radius = planet =>
-      Math.ceil(Math.sqrt(planet.power)) * 2;
+      Math.sqrt(planet.power * 0.01);
 
-const intersecting = (planet1, planet2) =>
-    Math.sqrt(planet1.x * planet1.x + planet2.y * planet2.y) < radius(planet1) + radius(planet2);
+const intersecting = planet1 => planet2 =>{
+    const dx = Math.abs(planet1.x - planet2.x);
+    const dy = Math.abs(planet1.y - planet2.y);
+    return Math.sqrt(dx * dx + dy * dy) < Math.sqrt(120000*0.01) * 2;
+};
 
-function createPlanets(n) {
-    return Array(n)
-        .fill(0)
-        .map(p => planet(randX(), randY(), randRate()));
+const intersectingPlanets = planets =>
+      planets
+      .map(p => planets
+           .map(intersecting(p))
+           .filter(identity)
+           .length)
+      .reduce((acc, val) => acc + val);
 
-}
+const createPlanets = n =>
+      [...Array(n)].map(p => planet(randX(), randY(), randRate()));
+
+const stop = () => planets = [];
+
